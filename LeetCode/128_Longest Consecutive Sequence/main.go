d5b396a7653151e5cc3f8ed18bce0213d4a44361
Georgiy Sokolov <ggranj@local>
2026-08-19 19:40:45 +0700

    leetcode 128 done

--- LeetCode/128_Longest Consecutive Sequence/main.go
+++ LeetCode/128_Longest Consecutive Sequence/main.go
@@ -1,5 +1,12 @@
 package main
 
+import "fmt"
+
+func main() {
+	res := longestConsecutive([]int{100, 1, 2, 3, 0})
+	fmt.Println(res)
+}
+
 func longestConsecutive(nums []int) int {
 	inputNumsSet := make(map[int]bool, len(nums))
 
@@ -11,6 +18,20 @@ func longestConsecutive(nums []int) int {
 
 	finalCounter := 0
 	for num := range inputNumsSet {
+		if _, exist := inputNumsSet[num-1]; exist { // если есть число меньше нынешнего на 1
+			continue
+		}
+		// здесь мы уже стоим на каком-то num, которое является самым маленьким в контексте непрерывной последовательности n+1, которую мы и будем считать далее
+
+		counter := 1
+		for inputNumsSet[num+1] {
+			num += 1
+			counter++
+		}
+
+		if counter > finalCounter {
+			finalCounter = counter
+		}
 	}
 
 	return finalCounter
